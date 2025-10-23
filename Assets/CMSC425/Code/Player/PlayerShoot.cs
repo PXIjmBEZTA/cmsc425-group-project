d4ac66f6fc1961526f37629addc5b4d8b5c1ab12
mahfufufu <54373990+mahfufufu@users.noreply.github.com>
2025-10-23 03:38:53 -0400

    Charged Shot and acceleration
    implemented a bigShoot) method that acts as a charged shot which accelerates the longer it lives

--- Assets/CMSC425/Code/Player/PlayerShoot.cs
+++ Assets/CMSC425/Code/Player/PlayerShoot.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -11,29 +12,79 @@ public class PlayerShoot : MonoBehaviour
     private float nextTimeFire = 0f;
     public ButtonControl shootButton;
 
+    public float counter = 0f;
+    private Vector3 scaleChange = new Vector3(.6f, .6f, .6f);
+
+    public bool isBig;
     private void Start()
     {
         shootButton = Mouse.current.leftButton;
+        //shootButton = Keyboard.current.mKey;
+        //shootButton = Keyboard.current.spaceKey;
     }
     // Update is called once per frame
     void Update()
     {
         if (nextTimeFire > 0)
-            nextTimeFire -= Time.deltaTime; 
+            nextTimeFire -= Time.deltaTime;
 
-        if (shootButton.isPressed && nextTimeFire <= 0)
+
+        if (shootButton.isPressed)
+            counter += Time.deltaTime;
+
+        if (shootButton.wasReleasedThisFrame)
         {
-            Shoot();
+            Debug.Log($"Counter on release: {counter}");
+            if (counter >= 1.2f)
+            {
+                Debug.Log("My charged shot is ready!!!!");
+                BigShoot();
+            }
+
+
+            else
+            {
+                Shoot();
+            }
+
+
+            counter = 0f; // reset after firing
             nextTimeFire = fireRate;
         }
 
+
+        // if (shootButton.isPressed && nextTimeFire <= 0)
+        // {
+        //     Shoot();
+        //     nextTimeFire = fireRate;
+        // }
+
+    }
+    
+    //a wrapper method that encapsulates both
+
+    public bool Shoot()
+    {
+        if (bulletPrefab != null && firePoint != null)
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+        isBig = false;
+        return true;
+        
     }
 
-    void Shoot()
+    public bool BigShoot()
     {
-        if (bulletPrefab == null || firePoint == null)
-            return;
+        if (bulletPrefab != null && firePoint != null)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            bullet.transform.localScale += scaleChange;  //modify this instance only
+            //otherwise bulletprefab.transform.localScale would modify the original prefab itself, permanently changing it
+        }
+        isBig = true;
+        return true;
 
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
     }
 }
--- Assets/CMSC425/Code/Projectiles/Player/PlayerBulletShoot.cs
+++ Assets/CMSC425/Code/Projectiles/Player/PlayerBulletShoot.cs
@@ -7,12 +7,17 @@ public class PlayerBulletShoot : MonoBehaviour, IPlayerBullet
     public float timeLeft;
     [SerializeField] private int damage;
     public int Damage => damage;
+
+     public float acceleration = 0.00076f;
+
+    private PlayerShoot shot;
     void Start()
     {
         speed = 3.0f;
         lifeTime = 5f;
         damage = 2;
         timeLeft = lifeTime;
+        shot = FindFirstObjectByType<PlayerShoot>();
     }
 
     
@@ -26,7 +31,15 @@ public class PlayerBulletShoot : MonoBehaviour, IPlayerBullet
         }
         else
         {
+            //no it serves no functional purpose other than looking cool
+            //(and visual feedback of a fast and powerful attack was shot just now)
+            if (shot.isBig) 
+                speed += acceleration*120 * Time.deltaTime*300;
+            //if false (regular small bullet) then it will move linearly without the accelerated speed
+
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
         }
     }
 
